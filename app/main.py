0bd845b296959f4e3a2089e0773324fd3538cd3e
Chamathka Prabahashana <chamath123pr@gmail.com>
2026-01-18 19:22:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-
+from fastapi import FastAPI
 from app.analyzer import analyze_text
+from app.models import TextInput
+
 app = FastAPI(title="CognitiveTrap - AI Deception Detector")
 
 @app.post("/analyze")
-def analyze(payload: dict):
-    text = payload.get("text", "")
-    return analyze_text(text)
+def analyze(input: TextInput):
+    return analyze_text(input.text)
